4e927d5ac74b34e2954f71d5acbb6772229787b9
Lauri Härsilä <murgo@iki.fi>
2013-03-19 11:20:39 +0200

    Created separate entry point just for kicks

---
+++ Scut/Scut/EntryPoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Scut
+{
+    internal class EntryPoint
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            App.Main();
+        }
+    }
+}
